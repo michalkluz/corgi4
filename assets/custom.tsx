f49b04402103e424d8cfc2fdd1a9964aa7615afa
Michał Kluz <michal@kluz.net>
2017-07-22 19:30:28 +0200

    new assets version

--- assets/custom.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="drzewko — kopia" tilewidth="32" tileheight="32" tilecount="25" columns="5">
- <image source="drzewko — kopia.png" width="185" height="185"/>
-</tileset>
--- assets/pien.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="pien" tilewidth="32" tileheight="32" tilecount="25" columns="5">
- <image source="pien.png" width="170" height="170"/>
-</tileset>
